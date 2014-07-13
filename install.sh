8daef59bad9e9de4970071cbb0970af06e3c790c
Ludovico Russo <ludus.russo@gmail.com>
2014-07-13 13:13:58 +0200

    install.sh updated

--- install.sh
+++ install.sh
@@ -13,6 +13,14 @@ files="vimrc gitconfig vim bashrc profile"    # list of files/folders to symlink
 
 ##########
 
+# remove (if it exist) the olddir
+if [ -d "$olddir" ]; then
+  echo "Removing $olddir"
+  rm -rf $olddir
+  echo "...done"
+fi
+
+
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
@@ -23,9 +31,11 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
+
+
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
+    echo "Moving ~/.$file $olddir/$file "
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
